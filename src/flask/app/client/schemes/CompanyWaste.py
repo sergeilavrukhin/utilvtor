1ce09ab52073c3a90231f7b2a4721025d79efc71
sergeilavrukhin <sergei.a.lavrukhin@gmail.com>
2021-02-20 00:13:52 +0500

    Fix companies top 3

--- src/flask/app/client/schemes/CompanyWaste.py
+++ src/flask/app/client/schemes/CompanyWaste.py
@@ -36,7 +36,7 @@ class CompanyByWasteClientSchema(ModelSchema):
         itn = el.itn
         company = db.session.query(models.Companies).filter(models.Companies.itn == itn).one_or_none()
         if company:
-            schema = CompanyWasteSchema()
+            schema = CompanyWasteSchema(only=("id", "name", "gps"))
             return schema.dump(company)
         else:
             return None
