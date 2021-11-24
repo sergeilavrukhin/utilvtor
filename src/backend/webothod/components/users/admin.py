fccf5dc8657342b249b2afa351862d6368ee0d7a
sergeilavrukhin <sergei.a.lavrukhin@gmail.com>
2021-11-24 10:58:23 +0500

    Front and backend django API

--- src/backend/webothod/components/users/admin.py
+++ src/backend/webothod/components/users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from .models import (
     UserProfile,
@@ -9,9 +11,9 @@ class UserInline(admin.StackedInline):
     model = UserProfile
 
 
-class UserAdmin(admin.ModelAdmin):
+class ExtUserAdmin(UserAdmin):
     inlines = [UserInline]
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, ExtUserAdmin)
